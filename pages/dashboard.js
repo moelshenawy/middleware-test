4b3318591e79d7f07007bbe4aa34991dfbb9b579
moelshenawy <mohamedgk5@gmail.com>
2023-02-24 19:17:08 +0200

    initial commit

---
+++ pages/dashboard.js
@@ -0,0 +1,35 @@
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+const Dashboard = () => {
+  const router = useRouter();
+
+  const logOut = () => {
+    Cookies.remove("loggedin");
+    router.push("/");
+  };
+
+  return (
+    <div>
+      <Link href="/">
+        <nav>
+          <h1>Home</h1>
+        </nav>
+      </Link>
+      <div className="dashboard">
+        <h1>Dashboard Page</h1>
+        <h1>You Are Logged In!</h1>
+        <button
+          type="submit"
+          onClick={() => logOut()}
+          style={{ backgroundColor: "#D93025" }}
+        >
+          Log Out
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
